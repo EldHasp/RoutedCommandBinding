b8d38fa70e3112b7bfee60674f2faa2e7377fee5
Хасп Элд <EldHasp@Gmail.com>
2020-05-02 18:08:42 +0300

    Создан класс AP-Property для привязки всплывающих RoutedComman к командам VM.

---
+++ RoutedCommandBindingSln/View/MyRoutedComands.cs
@@ -0,0 +1,13 @@
+﻿using System.Windows.Input;
+
+namespace View
+{
+	public static class MyRoutedComands
+	{
+		public static RoutedCommand First { get; }
+			= new RoutedUICommand("Первый", nameof(First), typeof(MyRoutedComands));
+
+		public static RoutedCommand Second { get; }
+			= new RoutedUICommand("Второй", nameof(Second), typeof(MyRoutedComands));
+	}
+}
---
+++ RoutedCommandBindingSln/View/ViewModel.cs
@@ -0,0 +1,16 @@
+﻿using System.Windows;
+using System.Windows.Input;
+
+namespace View
+{
+	public class ViewModel
+	{
+		public ICommand Command { get; } = new RelayCommand
+		(
+			p => MessageBox.Show((string)p),
+			p => !string.IsNullOrWhiteSpace(p as string)
+		);
+
+		public string Text { get; set; } = "Свойство";
+	}
+}
